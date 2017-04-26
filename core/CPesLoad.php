1edd6050827148913a6875942dc02fffa5fea389
Pavel A.D <aydoom@li.ru>
2017-04-26 21:56:20 +0300

    #change 02

--- core/CPesLoad.php
+++ core/CPesLoad.php
@@ -17,34 +17,71 @@ class CPesLoad {
 
     public $encode;
     public $redirect;
+	
+	private $html;
 
     
-    public function __construct($html, $encode = false, $redirect = true) {
+    public function __construct($html, $encode = false, $redirect = true)
+	{
         $this->encode = $encode;
         $this->redirect = $redirect;
         
+		
         if ($this->isUrl($html)) {
-            $url_string = trim($html);
-            $url = parse_url($url_string);
-            
-            $path_array = explode(".", $url['path']);
-            $url['type_file'] = end($path_array);
-            $url['url'] = $url_string;
-            // Проверяется на повторный запрос к хосту
-            CPesControl::sleep($url);
-            // Считываем страницу
-            $code = CPesCurl::load($url['url'],$encode,$redirect);
-        } else if ($this->isFile($html)) {
-            
-        } else if ($this->isHtml($html)){
-            
+			
+			$this->loadUrl($html);
+			
+        } elseif (is_file($html) ) {
+			
+            $this->loadFile($html);
+			
+        } elseif (strlen($html) > 0){
+			
+            $this->loadHtml($html);
+			
         }
     }
+
+
+	public function getHtml()
+	{
+		return $this->html;
+	}
     
-    
-    public function isUrl($html) {
+    public function isUrl($html)
+	{
         $url = parse_url(trim($html));
         return (isset($url['scheme']));
     }
-            
+	
+	
+	public function loadFile($fileName)
+	{
+		$this->html = file_get_contents($fileName);
+	}
+	
+	
+	public function loadHtml($html)
+	{
+		$this->html = $html;
+	}
+    
+	
+	public function loadUrl($url_string)
+	{
+		$url = parse_url(trim($url_string));
+		
+		$path_array = explode(".", $url['path']);
+		
+		$url['type_file'] = end($path_array);
+		
+		$url['url'] = $url_string;
+		
+		// Проверяется на повторный запрос к хосту
+		CPesControl::sleep($url);
+		
+		// Считываем страницу
+		$this->html = CPesCurl::load($url['url'], $this->encode, $this->redirect);
+		
+	}
 }
---
+++ core/CPesTag.php
@@ -0,0 +1,48 @@
+﻿<?php
+/**
+ * Основной класс библиотеки, создание объекта тега
+ */
+
+namespace Pes\Core;
+
+class CPesTag
+{
+	
+	public $id;
+	public $parentID;
+	
+	public $name;
+	public $type;
+	
+	public $selector = [];
+	
+	public $position = [];
+	
+	public $content;
+	
+	public $html;
+	
+	
+	public function __construct($html, $id, $parentID)
+	{
+		
+		$this->id = $id;
+		$this->parentID = $parentID;
+		
+		$this->setTag($html);
+		
+	}
+	
+	
+	public function setTag($html)
+	{
+		
+		foreach($html as $row => $tag) {
+		
+			
+		
+		}
+		
+	}
+	
+}
